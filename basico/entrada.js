884e9d823e09e115fe47f7190b8884accd132154
Lisboa <lisboadivalu@gmail.com>
2021-03-09 03:51:14 -0300

    Update

---
+++ basico/entrada.js
@@ -0,0 +1,7 @@
+document.write("<h1> Entrada Javascript</h1>");
+
+document.write("<img src='http://google.com/google.jpg' >")
+
+var nome = window.prompt("Digite seu nome para ver ");
+
+nome;
---
+++ basico/redirect.js
@@ -0,0 +1,48 @@
+function modify() {
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome');
+
+    if(texto.length == 0 || texto == null ) {
+        alert('Digite Seu Nome Para Prosseguir');
+        area.innerHTML = "Bem Vindo...";
+    } else {
+        area.innerHTML = "Bem Vindo " + texto;
+    }; 
+}
+
+var x = 0;
+
+while(x < 10) {
+    document.write( "</br> o valor de X: " + x);
+
+    x++;
+}
+
+
+document.write("</br></br> o valor de X: "+ x + "</br>");
+
+
+for(a = 0; a < x; a++) {
+    document.write("</br> o valor de A: " + a);
+}
+
+var y = 0;
+
+switch(y) {
+    
+    case 0:
+        alert("O Y vale 0!")
+        break
+    
+    case 1:
+        alert("O Y vale 1!")
+        break
+
+    case 2:
+        alert("O Y vale 2!")
+        break
+        
+    case 3:
+        alert("O Y vale 3!")
+        break    
+}
